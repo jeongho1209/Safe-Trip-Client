839afca2aeb81e6c51507cc304b5e4e98afd73e1
이정호 <dlwjdgh1115@gmail.com>
2024-03-25 11:20:36 +0900

    fix: commit lint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,36 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: { 'subject-case': [0] },
+    extends: [],
+    rules: {
+        'type-enum': [2, 'always'],
+    },
+    plugins: [
+        {
+            rules: {
+                'type-enum': ({ header }) => {
+                    const types = new Set([
+                        'build',
+                        'ci',
+                        'chore',
+                        'design',
+                        'docs',
+                        'add',
+                        'feat',
+                        'fix',
+                        'perf',
+                        'refactor',
+                        'style',
+                        'test',
+                        'revert',
+                        'merge',
+                        'project',
+                        'delete',
+                    ]);
+                    return [
+                        types.has(header.split(' :: ')[0]),
+                        `${header.split(' :: ')[0]}은 type으로 사용할 수 없습니다.`,
+                    ];
+                },
+            },
+        },
+    ],
 };
