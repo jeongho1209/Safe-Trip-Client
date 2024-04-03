e0c85e7c9f77a75582c584c87fef2612d5798820
이정호 <dlwjdgh1115@gmail.com>
2024-04-03 16:15:22 +0900

    feat: TravelInfo

--- src/pages/travelInfo.jsx
+++ src/pages/travelInfo.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/header/index.jsx';
+import { TravelInfoText } from '../components/travelInfo/text/index.jsx';
 
 export const TravelInfo = () => {
     return (
         <>
             <Header></Header>
+            <TravelInfoText></TravelInfoText>
         </>
     );
 };
